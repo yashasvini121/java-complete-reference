238aa41f1a2cdfc13e911e535c7d9d8f96a36ade
yashasvini <ysmarina1234@gmail.com>
2024-01-28 14:22:13 +0530

    documentation comment

---
+++ Part 1/Chapter 2/DocumentationComment.java
@@ -0,0 +1,11 @@
+/**
+ * This class demonstates the use of Documentation Comments.
+ * @author Yashasvini Sharma
+ * @version 1.0
+ */
+
+public class DocumentationComment {
+	public static void main(String[] args) {
+		
+	}
+}
