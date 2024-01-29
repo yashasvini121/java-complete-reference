50cfa23a5e999b234bdd01b6f31f5c0a797a40fe
yashasvini <ysmarina1234@gmail.com>
2024-01-29 23:48:12 +0530

    type interface

---
+++ Part 1/Chapter 3/TypeInference.java
@@ -0,0 +1,8 @@
+public class TypeInference {
+	public static void main(String[] args) {
+		// var is a context-sensitive identifier and so int var = 5 (user defined) is also valid 
+		var age = 10;
+		System.out.println(age);
+	}
+}
+
